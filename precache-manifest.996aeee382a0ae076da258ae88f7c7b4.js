60d436051856bb77496539be2349066144424168
Emilia Allison <contact@emiliaallison.com>
2020-02-11 11:59:59 -0500

    Updates

---
+++ precache-manifest.996aeee382a0ae076da258ae88f7c7b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c5682598cb0eb2a623420bd37a9a859",
+    "url": "/series-test/index.html"
+  },
+  {
+    "revision": "dbed6cda646e0e8d0ccc",
+    "url": "/series-test/static/css/main.81fa44f7.chunk.css"
+  },
+  {
+    "revision": "8f5f24ff4e2bb1590632",
+    "url": "/series-test/static/js/2.6453e8b3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/series-test/static/js/2.6453e8b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbed6cda646e0e8d0ccc",
+    "url": "/series-test/static/js/main.37cf5f53.chunk.js"
+  },
+  {
+    "revision": "f041db373650c4602164",
+    "url": "/series-test/static/js/runtime-main.f7993dd8.js"
+  }
+]);
